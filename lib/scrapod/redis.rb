4e365869f0aba50811618a8c4a9abc52e1a73db8
krowpu <krowpu@tightmail.com>
2017-03-23 03:55:12 +0500

    Add class Scrapod::Redis::Process

--- lib/scrapod/redis.rb
+++ lib/scrapod/redis.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'scrapod/redis/process'
 require 'scrapod/redis/session'
 require 'scrapod/redis/version'
 
---
+++ lib/scrapod/redis/process.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'scrapod/redis/base'
+
+module Scrapod
+  module Redis
+    class Process
+      self.model_name = 'process'
+
+      attr_reader :started_at
+
+      def initialize(*)
+        super
+
+        @started_at ||= nil
+      end
+
+      def started_at=(value)
+        @started_at = value.nil? ? nil : Time.at(value)
+      end
+
+      def as_json
+        {
+          'started_at' => started_at&.to_i,
+        }
+      end
+    end
+  end
+end
