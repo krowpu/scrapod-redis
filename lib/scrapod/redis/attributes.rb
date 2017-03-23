7cc9373378ad3e5e7baaba873c77bf192d74384f
krowpu <krowpu@tightmail.com>
2017-03-23 08:07:05 +0500

    Fix code

--- lib/scrapod/redis/attributes.rb
+++ lib/scrapod/redis/attributes.rb
@@ -42,7 +42,7 @@ module Scrapod
       class Datetime < Base
         def typecast(value)
           return               if value.nil?
-          return value         if value.is_a? Time
+          return value         if value.is_a? ::Time
           return Time.at value if value.is_a?(::Integer) || value.is_a?(::Float)
           raise TypeError
         end
